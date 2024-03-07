463ad1ae6a8b5cc9b3757805154eea92efd54092
Andrey-Fedosov <andrefedosov@gmail.com>
2024-03-08 01:58:53 +0300

    codewars: add 3 katas for 08.03.2024

--- codeWarsKataStore.js
+++ codeWarsKataStore.js
@@ -15,3 +15,51 @@ function initials(n) {
 }
 
 //* other user variant
+//!  should add some other user variants
+
+//& 08.03.2024
+
+//^ 1. to split a given string into different strings of equal size (note size of strings is passed to the method)
+
+//* my variant
+
+let splitInParts = function (s, partLength) {
+  if (s.length < partLength) {
+    return s;
+  } else {
+    let splittedSArray = [];
+    for (let i = 0; i < s.length; i += partLength) {
+      splittedSArray.push(s.slice(i, i + partLength));
+    }
+    return splittedSArray.join(" ");
+  }
+};
+
+//* other user variant
+//!  should add some other user variants
+
+//^ 2.  function receives an array of integers as input. Your task is to determine whether the numbers are in ascending order.
+
+//* my variant
+function inAscOrder(arr) {
+  const stringInOrder = [...arr].sort((a, b) => a - b).join("");
+  return arr.join("") === stringInOrder;
+}
+
+//* other user variant
+//!  should add some other user variants
+
+//^3 you get the start number and the end number of a region and should return the count of all numbers except numbers with a 5 in it
+
+//* my variant
+function dontGiveMeFive(start, end) {
+  let rowFromStartToEnd = [];
+  for (let i = start; i <= end; i++) {
+    rowFromStartToEnd.push(i.toString());
+  }
+
+  return rowFromStartToEnd.filter((el) => !el.includes("5")).length;
+}
+
+//* other user variant
+//!  should add some other user variants
